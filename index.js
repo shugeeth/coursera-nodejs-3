a560c1dac7df9869a7a94447d56c265c5dabff46
Shugeeth Babu <meetshugeeth@gmail.com>
2021-06-12 16:40:07 +0530

    Week 1 Assignment - Different Routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
 const dishRouter = require('./routes/dishRouter')
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
 
 const hostname = 'localhost'
 const port = 3000
@@ -12,6 +14,8 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use('/dishes', dishRouter)
+app.use('/promotions', promoRouter)
+app.use('/leaders', leaderRouter)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
